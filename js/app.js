b9401eef7d385c0d74d6eb108f7df1fc964c3323
nasiratt92 <nasiratt92@gmail.com>
2018-07-16 15:55:33 +0100

    manage to create click reponse on random tiles

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 $(() => {
   const colors = ['tan', 'teal', 'coral', 'peru', 'tomato', 'honeydew', 'sienna',
-    'ordchid', 'ivory', 'crimson', 'azure', 'indigo', 'purple'];
-  const display = $('.squares');
+    'ordchid', 'ivory', 'crimson', 'azure', 'indigo', 'purple', 'dodgerblue', 'goldenrod'];
+  const $display = $('.squares');
 
-  const answer = $('#choosen').text();
-
-  const $tiles = $('.tile');
 
   function randomUpTo(n) {
     return Math.floor(Math.random()*n);
   }
+
+  const answer = (colors) => {
+    const answerIndex = randomUpTo(colors.length);
+    const answerColor = colors[answerIndex];
+    return answerColor;
+  // answerColor.('#choosen').text();
+  };
+
+  // const color = colors[colorNumber];
 
   //This secton is to genrate a array of four colors from the weird color names///////
   const pickRandomColors = (colors, n) => {
@@ -32,29 +38,60 @@ $(() => {
     while(!pickedColors.includes(mustContain)) {
       pickedColors = pickRandomColors(colors, n);
       //  Display picked colors for a little while
-      console.log(pickedColors);
+      //only way is to run  intervals before the loop
+      console.log('picking...', pickedColors);
     }
     return pickedColors;
 
   };
   ///////////////////////////////////////////////////////////////////////////////////
-  console.log(pickColorsUntilContains(colors, 3, answer));
+  // console.log(pickColorsUntilContains(colors, 3, answer));
+
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  function appendNDivsToDisplay(n) {
+    const pickedColors = pickColorsUntilContains(colors, n, answer);
+    console.log('colors picked', pickedColors);
+    pickedColors.forEach((color) => {
+      if (n===4){
+        $display.append(`<div class="tile" id="${color}" style="background-color: ${color}; height: 190px; width: 190px;"></div>`);
+      } else if (n===9){
+        $display.append(`<div class="tile" id="${color}" style="background-color: ${color}; height: 120px; width: 120px;"></div>`);
+      } else if (n===16){
+        $display.append(`<div class="tile" id="${color}" style="background-color: ${color}; height: 90px; width: 90px;"></div>`);
+      }
+    });
+  }
+
+
+  const level1 = appendNDivsToDisplay(4);
+
+  const $tiles = $('.tile');
+  // const level2 = appendNDivsToDisplay(9);
+
+  // const level3 = appendNDivsToDisplay(16);
+
+  //this section to get diferent alerts on user selection////
 
   $tiles.on('click',(e)=>{
     const guess = $(e.target).attr('id');
+// console.log(guess);
     if (guess === answer)
       alert(`this is ${answer} mate`);
     else alert(`Ha Ha ! Gotcha this is not ${answer} mate try again`);
   });
-//////////////////////////////////////////////////////////////////////////////
 
 
 
-  // colors.forEach((color) => {
-  //   console.log(color);
-  // });
 
-  console.log(answer);
+  // console.log(appendNDivsToDisplay(4));
+
+  // $display.on('click', div => {
+  //
+  // })
+
+  // console.log(answer);
 
 
 });
